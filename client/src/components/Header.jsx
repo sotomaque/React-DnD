2690ca5963111ded8f72575c5912aa82c975bce8
sotomaque <sotomaque@gmail.com>
2020-05-10 17:24:48 -0700

    first commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className={"row"}>
+            <p className={"page-header"}>Project Dashboard ✔️</p>
+        </div>
+    )
+}
+export default Header; 
